8463f1317772f0914416a2c681a634c1f1e05c53
jongwon <jjongwon7@naver.com>
2021-11-21 16:51:09 +0900

    addImage

--- app/src/main/java/com/example/vocaproject/BackgroundSettingActivity.java
+++ app/src/main/java/com/example/vocaproject/BackgroundSettingActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class BackgroundSettingActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
