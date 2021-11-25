6ea76a7c519ff56d03515ec83613ed6fb3a81033
jongwon <jjongwon7@naver.com>
2021-11-25 10:31:00 +0900

    addToken

--- app/src/main/java/com/example/vocaproject/LoginActivity.java
+++ app/src/main/java/com/example/vocaproject/LoginActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -49,7 +50,8 @@ public class LoginActivity extends UserAccount {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // 로그인 성공
-                            currentID = strEmail;
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            currentID = firebaseUser.getUid();
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish(); // 현재 액티비티 파괴
