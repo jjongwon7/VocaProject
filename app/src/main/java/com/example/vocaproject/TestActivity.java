fd1a04666ba4c08aad6dc61923387d6b4fc90a96
mmalfoy <93825669+mmalfoy@users.noreply.github.com>
2021-12-07 10:16:28 +0900

    테스트 업뎃-인텐트 남음

--- app/src/main/java/com/example/vocaproject/TestActivity.java
+++ app/src/main/java/com/example/vocaproject/TestActivity.java
@@ -17,29 +17,36 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView.OnEditorActionListener; //OnEditorActionListener import하기
-
+import java.util.ArrayList;
 import android.widget.TextView;
 import android.widget.Toast;
 
+//로그인 정보 처리??
+//단어 개수에 따른 프로그래스바 처리
 
-public class TestActivity extends AppCompatActivity implements OnEditorActionListener{ //OnEditorActionListener 인터페이스 implement하기
+public class TestActivity extends UserAccount implements OnEditorActionListener{ //OnEditorActionListener 인터페이스 implement하기
 
     private String key;
     private EditText inputText;
     ProgressBar progressBar;
     TextView textview;
-    TextView tv[] = new TextView[3];
-    TextView rs[] = new TextView[3];
+    int num_of_word = 4; // 각 챕터 단어 개수
+    TextView t_mean;
+    TextView rs[] = new TextView[num_of_word];
     TextView Mean;
+    TextView TV_1;
     TextView test_score;
     LinearLayout result;
     FrameLayout meanFrame;
-    String str1 = "passport";
-    String str2 = "airport";
-    String str3 = "boarding pass";
+    LinearLayout Ly;
+
+    String str1 ;
+
+    ArrayList<Word> arrayDB;
     int index = 0; // 단어 참조 인덱스
-    int correct[] = new int [3]; //맞은놈 틀린놈 - 작업 필요
+    int correct[] = new int [num_of_word]; //맞은놈 틀린놈 - 작업 필요
     int score;  //단어 갯수에 따른 아래 값 수정 필요
+    int progress = (100/4); // 테스트 진도율 -수정 필요
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,30 +62,51 @@ public class TestActivity extends AppCompatActivity implements OnEditorActionLis
         test_score = (TextView)findViewById(R.id.result_test);
 //        String inputStr = inputText.getText().toString().trim();
 
-        tv[0] = (TextView)findViewById(R.id.textView1);
-        tv[1] = (TextView)findViewById(R.id.textView2);
-        tv[2] = (TextView)findViewById(R.id.textView3);
+        t_mean = (TextView)findViewById(R.id.textView1);
+
 
         Mean = (TextView)findViewById(R.id.Mean);
         result = (LinearLayout) findViewById(R.id.result);
-        meanFrame =(FrameLayout) findViewById(R.id.meanFrame);
+        Ly = (LinearLayout) findViewById(R.id.LY1);
+
 
         rs[0] = (TextView)findViewById(R.id.testresult1);
         rs[1] = (TextView)findViewById(R.id.testresult2);
         rs[2] = (TextView)findViewById(R.id.testresult3);
+        rs[3] = (TextView)findViewById(R.id.testresult4);
+        TV_1 = (TextView)findViewById(R.id.Tv_1);
 
-        for(int i = 0; i < 3; i++) {
+
+
+        for(int i = 0; i < num_of_word; i++) {
             correct[i] = 1;
         }
 
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
 //        int input = Integer.parseInt(inputStr);
+        TV_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //처음 화면 터치 후 테스트 실행
+                TV_1.setVisibility(View.INVISIBLE);
+                Ly.setVisibility(View.VISIBLE);
 
+                for(int i=0;i < num_of_word;i++){
+                    arrayList.get(i).getEnglish();
+                    arrayList.get(i).getKoreanMean();
+                }
+                t_mean.setText(arrayList.get(index).getKoreanMean());
+                progressBar.setProgress(progress);
+
+            }
+        });
     }
 
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         //오버라이드한 onEditorAction() 메소드
+
+
 
         String strText;
         strText= inputText.getText().toString() ;
@@ -92,74 +120,48 @@ public class TestActivity extends AppCompatActivity implements OnEditorActionLis
             }
             else {
 
-                switch (index) {
+                if(index<num_of_word ){
                     //각각의 테스트 입력결과 (정답, 오답)확인 후 다음 단어로 전환
-                    case 0:
-                        progressBar.setProgress(10);
-                        if(strText.equals(str1)){
-                            score+=10;
+                        progressBar.setProgress(progress);
+                        str1 = arrayList.get(index).getEnglish();
+//                        tv[0].setText(""+str1);
+                        if (strText.equals(str1)) {
+                            score += 10;
                             showToastcorr();
-                        }
-                        else {
+                        } else {
                             correct[index] = 0;
                             showToastincorr();
 
                         }
                         inputText.setText(null);
-                        tv[index].setVisibility(View.GONE);
-                        tv[index+1].setVisibility(View.VISIBLE);
                         index++;
-                        break;
+                        if(index<num_of_word)
+                            t_mean.setText(arrayList.get(index).getKoreanMean());
+                        progress += (100 / num_of_word);
 
-                    case 1:
-                        progressBar.setProgress(20);
-                        if(strText.equals(str2)){
-                            score+=10;
-                            showToastcorr();
-                        }
-                        else {
-                            correct[index] = 0;
-                            showToastincorr();
-                        }
-                        inputText.setText(null);
-                        tv[index].setVisibility(View.GONE);
-                        tv[index+1].setVisibility(View.VISIBLE);
-                        index++;
-                        break;
-
-                    case 2:
-                        progressBar.setProgress(30);
-                        if(strText.equals(str3)){
-                            score+=10;
-                            showToastcorr();
-                        }
-                        else {
-                            correct[index] = 0;
-                            showToastincorr();
-                        }
-
-                        inputText.setText(null);
-                        inputText.setVisibility(View.GONE);
-                        Mean.setVisibility(View.GONE);
-                        meanFrame.setVisibility(View.GONE);
-                        //마지막 단어 테스트 후 결과 화면 출력
-                        result.setVisibility(View.VISIBLE);
-                        textview.setText("정답률");
-                        progressBar.setProgress(score);
-                        test_score.setText("User의 Part1 점수"+score+"점");
-                        //오답인 애들 스크롤 뷰
-                        for(int i=0; i<3 ; i++){
-                            if(correct[i] == 0){
-                                rs[i].setVisibility(View.VISIBLE);
+                        if(index == num_of_word ){
+                            inputText.setVisibility(View.GONE);
+                            Mean.setVisibility(View.GONE);
+                            t_mean.setVisibility(View.GONE);
+                            //마지막 단어 테스트 후 결과 화면 출력
+                            result.setVisibility(View.VISIBLE);
+                            textview.setText("정답률");
+                            progressBar.setProgress(score);
+                            test_score.setText("User의 Part1 점수" + score + "점");
+                            //오답인 애들 스크롤 뷰
+                            for (int i = 0; i < num_of_word; i++) {
+                                if (correct[i] == 0) {
+                                    rs[i].setVisibility(View.VISIBLE);
+                                    rs[i].setText("\n"+arrayList.get(i).getEnglish()+arrayList.get(i).getKoreanMean());
+                                }
                             }
                         }
-                        break;
-
                 }
             }
         }
        return false;
     }
+
     //커스텀 토스트 함수
     public void showToastcorr(){
         //editText로 사용자로부터 입력받은 문자열이 참일때 출력하는 토스트 메시지
